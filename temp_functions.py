006b6969a27a5ec5b16dca4dbd83b9b26c1ed8f7
hirodotm <>
2021-04-26 01:27:02 +0000

    I added docstrings in temp_functions.py

--- temp_functions.py
+++ temp_functions.py
@@ -1,8 +1,16 @@
+#functions relate temperature calculation in problem 3
+
+#define fahr_to_celsius
 def fahr_to_celsius(temp_fahrenheit):
+#input: temperature in fahrenheit
+#output: temperature in celsius from fahrenheit by function
   converted_temp=(temp_fahrenheit-32)*5/9
   return converted_temp
 
+#define temp_classifier
 def temp_classifier(temp_celsius):
+#input: temperatures in celsius
+#output: number of class which is devided by given rules
   if(temp_celsius<-2):
    return 0
   elif((temp_celsius>=-2)and(temp_celsius<2)):
